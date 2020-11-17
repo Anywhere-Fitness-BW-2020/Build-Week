b8604065d49a4afba173504f0eec14d24ef14f84
Robert Petersen <robert.petersen808@gmail.com>
2020-11-17 13:54:59 -0600

    styled completed

--- src/robs-components/Sign Ins/ClientSignIn.js
+++ src/robs-components/Sign Ins/ClientSignIn.js
@@ -1,18 +1,18 @@
 import React from "react";
 import FormClient from "../Forms/FormClient";
+import Styled from "styled-components";
 
 export default function ClientSignIn(props){
   const { formData, disabled, formSubmit, handleChanges, errors } = props;
 
 
   return(
-    <div>
+    <ClientSignInStyles>
 
-      <div>
-        -- image here on left side --
+      <div className="side-image">
       </div>
 
-      <div>
+      <div className="form-container">
 
         <div>
           <h2>Client<br/>Sign In</h2>
@@ -27,6 +27,35 @@ export default function ClientSignIn(props){
         />
       </div>
 
-    </div>
+    </ClientSignInStyles>
   )
 }
+
+const ClientSignInStyles = Styled.div`
+  display: flex;
+  Justify-content: space-evenly;
+  align-items: center;
+
+  .side-image{
+    background-image: url("https://cdn2.stylecraze.com/wp-content/uploads/2013/11/Zen-Yoga-%E2%80%93-What-Is-It-And-What-Are-Its-Benefits-2-624x702.jpg");
+    min-height: 80vh;
+    min-width: 50vw;
+    max-width: 50vw;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .form-container{
+    display: flex;
+    flex-direction: column;
+    Justify-content: space-evenly;
+    align-items: center;
+
+    h2{
+      font-size: 3rem;
+      text-align: center;
+      margin: 30px;
+    }
+  }
+`;
--- src/robs-components/Sign Ins/InstructorSignIn.js
+++ src/robs-components/Sign Ins/InstructorSignIn.js
@@ -1,18 +1,18 @@
 import React from "react";
 import FormInstructor from "../Forms/FormInstructor";
+import Styled from "styled-components";
 
 export default function InstructorSignIn(props){
   const { formData, disabled, formSubmit, handleChanges, errors } = props;
 
 
   return(
-    <div>
+    <InstructorSignInStyles>
 
-      <div>
-        -- image here on left side --
+      <div className="side-image">
       </div>
 
-      <div>
+      <div className="form-container">
 
         <div>
           <h2>Instructor<br/>Sign In</h2>
@@ -27,6 +27,35 @@ export default function InstructorSignIn(props){
         />
       </div>
 
-    </div>
+    </InstructorSignInStyles>
   )
 }
+
+const InstructorSignInStyles = Styled.div`
+  display: flex;
+  Justify-content: space-evenly;
+  align-items: center;
+
+  .side-image{
+    background-image: url("https://www.trainerize.com/blog/wp-content/uploads/2019/02/Fitness_for_2_selling_personal_training_to_couples.jpg");
+    min-height: 80vh;
+    min-width: 50vw;
+    max-width: 50vw;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .form-container{
+    display: flex;
+    flex-direction: column;
+    Justify-content: space-evenly;
+    align-items: center;
+
+    h2{
+      font-size: 3rem;
+      text-align: center;
+      margin: 30px;
+    }
+  }
+`;
